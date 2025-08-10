ed69dbb00c5c619549567922f8072f86c0fbbcc6
Halil Yildirim <ce.hybusiness@gmail.com>
2025-08-10 12:29:47 +0300

    Events have been created and the next step is to consume them.

---
+++ Shared/Events/StockEvents/StockNotReservedEvent.cs
@@ -0,0 +1,21 @@
+﻿using MassTransit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Events.StockEvents
+{
+    public class StockNotReservedEvent : CorrelatedBy<Guid>
+    {
+        public StockNotReservedEvent(Guid correlationId)
+        {
+            CorrelationId = correlationId;
+
+        }
+        public Guid CorrelationId { get; }
+        public string Message { get; set; }
+
+    }
+}
